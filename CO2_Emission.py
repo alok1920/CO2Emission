b9f86775da2a4e2633c5e8ecd3dc972c544d2eea
alok1920 <59192984+alok1920@users.noreply.github.com>
2021-07-23 23:24:58 +0530

    Initial commit

---
+++ CO2_Emission.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+from sklearn.ensemble import BaggingClassifier
+from sklearn.experimental import enable_iterative_imputer
+from sklearn.impute import IterativeImputer
+from sklearn.ensemble import RandomForestRegressor
+
+st.title('CO2 Emission')
+
+st.sidebar.header('User Input Parameter')
