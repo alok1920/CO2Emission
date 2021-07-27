a819617bf197444f0734b5dde942ccd891f9328e
alok1920 <59192984+alok1920@users.noreply.github.com>
2021-07-27 10:39:08 +0530

    Done with deployment

--- CO2_Emission.py
+++ CO2_Emission.py
@@ -5,6 +5,8 @@ from statsmodels.tsa.seasonal import seasonal_decompose
 from sklearn.metrics import mean_squared_error
 from math import sqrt
 from statsmodels.tsa.arima_model import ARIMA
+import plotly.figure_factory as ff
+import plotly.graph_objects as go
 
 
 st.title('CO2 Emission')
@@ -12,13 +14,16 @@ st.title('CO2 Emission')
 st.header('User Input Parameter')
 CO2 = st.slider('Till waht year you like to see CO2 emission', 2015, 2065)
 number = CO2 - 2014
-st.write(number)
+#st.write(number)
 
+#df = pd.read_excel('CO2 dataset.xlsx', header=0, index_col=0, parse_dates=True)
 
+#adding training data
+#train = pd.read_csv('dataset.csv', header=None, index_col=0,parse_dates=True, squeeze=True)
 # fit model
-train=pd.read_excel('CO2 dataset.xlsx', header=0, index_col=0, parse_dates=True)
+data=pd.read_excel('CO2 dataset.xlsx', header=0, index_col=0, parse_dates=True)
 # prepare data
-X = train.values
+X = data.values
 X = X.astype('float32')
 
 model = ARIMA(X, order=(3,1,0))
@@ -26,4 +31,5 @@ model_fit = model.fit()
 
 forecast=model_fit.forecast(steps=number)[0]
 
-st.write(forecast)
+st.write("The CO2 Emission of the Year you entered", forecast[number - 1])
+
