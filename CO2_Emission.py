73b964061abb752a3dc86abfa57fd8805584b976
alok1920 <59192984+alok1920@users.noreply.github.com>
2021-07-28 11:13:27 +0530

    Update CO2_Emission.py

--- CO2_Emission.py
+++ CO2_Emission.py
@@ -5,7 +5,7 @@ from statsmodels.tsa.seasonal import seasonal_decompose
 from sklearn.metrics import mean_squared_error
 from math import sqrt
 from statsmodels.tsa.arima_model import ARIMA
-import plotly.figure_factory as ff
+import plotly.express as px
 import plotly.graph_objects as go
 import matplotlib.pyplot as plt
 
@@ -17,10 +17,17 @@ CO2 = st.slider('Select Year', 2015, 2065)
 number = CO2 - 2014
 #st.write(number)
 
-#df = pd.read_excel('CO2 dataset.xlsx', header=0, index_col=0, parse_dates=True)
+#Create a list
+ListCO2 = []
+for i in range(2015, 2065):
+    ListCO2.append(i)
+#save all the years in the list till the user has selected
+N = CO2 + 1
+temp = ListCO2.index(N)
+res = ListCO2[:temp]
+res_array = np.array(res)
+#st.write(type(res_array))
 
-#adding training data
-#train = pd.read_csv('dataset.csv', header=None, index_col=0,parse_dates=True, squeeze=True)
 # fit model
 data=pd.read_excel('CO2 dataset.xlsx', header=0, index_col=0, parse_dates=True)
 # prepare data
@@ -31,6 +38,15 @@ model = ARIMA(X, order=(3,1,0))
 model_fit = model.fit()
 
 forecast=model_fit.forecast(steps=number)[0]
-
+#st.write(type(forecast))
 st.write("The CO2 Emission of the Year you entered", forecast[number - 1])
+
+#dataframe of a table for chart.
+
+#create a table to display values.
+fig = go.Figure(data=[go.Table(header=dict(values=['Year', 'CO2'],fill_color = '#FD8E72',align = 'center', font_color = 'black'),
+                 cells=dict(values=[res_array, forecast],fill_color = '#E5ECF6',align = 'left',font_color = 'black'))
+                     ])
+fig.update_layout(margin=dict(l=5,r=5,b=10,t=10))
+st.write(fig)
 
